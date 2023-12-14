6e57d0be13ac209fd83f4a30dfa108ba6e7bacb3
pro-game-developer10 <74665178+pro-game-developer10@users.noreply.github.com>
2023-12-14 23:03:58 +0200

    last minute changes before sleep

--- src/listeners/supportVC.ts
+++ src/listeners/supportVC.ts
@@ -13,7 +13,6 @@ export class SupportVCListener extends Listener {
         });
     }
     public async run(_oldState: VoiceState, newState: VoiceState) {
-        console.log(ConfigUtils.findOneMentionableByFlags("channel", "WFS_CHANNEL"))
         if (!(newState.channelId! == ConfigUtils.findOneMentionableByFlags("channel", "WFS_CHANNEL").id)) return;
         const supportChannelCount = newState.guild.channels.cache.filter(
             (channel) =>
